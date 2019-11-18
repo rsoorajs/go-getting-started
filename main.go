10845a3de240ed782b83c64f99686a3bf0826828
SOORAJ S R <rsoorajs@gmail.com>
2019-11-18 15:15:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,82 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+// Pastebin uploader
+func PasteBinBot() {
+	b, err := tb.NewBot(tb.Settings{
+		Token:  os.Getenv("SECRET_TOKEN_PB_BOT"),
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 
-	router.Run(":" + port)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Get pastebin private token key
+	pasteBinToken := os.Getenv("PASTEBIN_TOKEN")
+
+	if pasteBinToken == "" {
+		log.Fatal("PASTEBIN_TOKEN env is missing")
+		return
+	}
+
+	// Options
+	//private := 1 // 1 = unlisted, 0 = public, 2 = private
+	pasteBinPostUrl := "https://pastebin.com/api/api_post.php"
+	pasteExpiration := "1D"
+
+	// The bot handlings
+	b.Handle("/help", func(m *tb.Message) {
+		b.Send(m.Sender, "Pastebin bot")
+	})
+
+	b.Handle(tb.OnText, func(m *tb.Message) {
+		if m.Text != "" {
+			formData := url.Values{
+				"api_dev_key":    {pasteBinToken},
+				"api_option":     {"paste"},
+				"api_paste_code": {m.Text},
+				//"api_paste_private": {private},
+				"api_paste_expire_date": {pasteExpiration},
+			}
+			resp, err := http.PostForm(pasteBinPostUrl, formData)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer resp.Body.Close()
+
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Print(string(body))
+			if resp.StatusCode == 200 {
+				b.Send(m.Sender, string(body))
+			}
+
+		}
+	})
+
+	b.Handle(tb.OnQuery, func(q *tb.Query) {
+	})
+
+	b.Start()
+}
+
+func main() {
+	go PasteBinBot()
+	select {}
 }
